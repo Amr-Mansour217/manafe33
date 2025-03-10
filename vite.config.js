d2b650be3891262acb69db33072888da8a0deca9
Amr Mansour <amrmansour217@gmail.com>
2025-03-10 21:51:26 +0200

    fix: update base path in vite.config.js for GitHub repository

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/manafe33/', // Add this line
+  base: '/manafe33/' // 👈 استبدل repository-name باسم مستودعك على GitHub
 })
